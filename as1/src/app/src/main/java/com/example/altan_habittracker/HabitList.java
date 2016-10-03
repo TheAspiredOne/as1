d4f025af742f61fab153d6daa3aefeffd1a207aa
Avery Tan <altan@ualberta.ca>
2016-10-03 15:41:37 -0600

    comments

--- as1/src/app/src/main/java/com/example/altan_habittracker/HabitList.java
+++ as1/src/app/src/main/java/com/example/altan_habittracker/HabitList.java
@@ -5,18 +5,22 @@ import java.util.ArrayList;
 /**
  * Created by Avery on 10/2/2016.
  */
+//list that stores instances of habits
 public class HabitList {
+    //constructor
     public ArrayList<Habit> myNewHabitList = new ArrayList<Habit>();
 
-
+    //return the list of current actively recorded habits
     public ArrayList<Habit> getHabitList() {
         return myNewHabitList;
     }
 
+    //adds a habit to habitlist so it can be tracked
     public void addHabit(Habit newHabit) {
         myNewHabitList.add(newHabit);
     }
 
+    //returns whether a habit is contained within a habitlist
     public boolean contains(Habit newHabit) {
         return myNewHabitList.contains(newHabit);
     }
