3d332148703c3777b2dceb94b7b9b4985b6e26f2
Avery Tan <altan@ualberta.ca>
2016-10-03 15:40:27 -0600

    comments

--- as1/src/app/src/main/java/com/example/altan_habittracker/HabitController.java
+++ as1/src/app/src/main/java/com/example/altan_habittracker/HabitController.java
@@ -3,6 +3,7 @@ package com.example.altan_habittracker;
 /**
  * Created by Avery on 10/2/2016.
  */
+//supposed to be a controller
 public class HabitController {
     private static HabitList habitList = null;
     static public HabitList getHabitList(){
